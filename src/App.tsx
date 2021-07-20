75081e7e937b6f1f0d97175541b247f09d442ed2
Glen Shaji <glenkshaji@gmail.com>
2021-07-20 18:36:26 +0300

    added local storage for persisting data on a new render

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,19 @@ export interface Note {
 function App() {
   const [notes, setNotes] = useState<IState['notes']>([]);
 
+  // everytime on a re render, we want to store what we have in localstorage to our setnotes state
+  React.useEffect(() => {
+    const data = localStorage.getItem('my-notes');
+    if (data) {
+      setNotes(JSON.parse(data));
+    }
+  }, []);
+
+  // storing the local storage
+  React.useEffect(() => {
+    localStorage.setItem('my-notes', JSON.stringify(notes));
+  });
+
   const onAdd = (note: Note) => {
     setNotes((prevNotes) => {
       return [...prevNotes, note];
